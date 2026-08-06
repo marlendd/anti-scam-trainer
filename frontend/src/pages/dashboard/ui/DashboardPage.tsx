7ad29d60a6dfddb8b579c19704cbc226ea5e36ab
Егор Лапшин <https.abuser@proton.me>
2026-08-06 14:08:22 +0300

    feat: add customer & seller charts

--- frontend/src/pages/dashboard/ui/DashboardPage.tsx
+++ frontend/src/pages/dashboard/ui/DashboardPage.tsx
@@ -1,21 +1,21 @@
-import { useDocumentTitle } from '@/shared/lib/use-document-title'
-import { DashboardBalanceChart } from '@/widgets/dashboard-balance-chart'
-import { DashboardHeatMap } from '@/widgets/dashboard-heat-map'
-import { DashboardOverview } from '@/widgets/dashboard-overview'
-import { DashboardRatingChart } from '@/widgets/dashboard-rating-chart'
-import { DashboardScenariosBarChart } from '@/widgets/dashboard-scenarios-bar-chart'
+import {useDocumentTitle} from '@/shared/lib/use-document-title'
+import {DashboardBalanceChart} from '@/widgets/dashboard-balance-chart'
+import {DashboardOverview} from '@/widgets/dashboard-overview'
+import {DashboardRatingChart} from '@/widgets/dashboard-rating-chart'
+import {DashboardScenariosBarChart} from '@/widgets/dashboard-scenarios-bar-chart'
 import styles from './DashboardPage.module.scss'
+import {DashboardPathProgress} from "@/widgets/dashboard-path-progress";
 
 export function DashboardPage() {
-  useDocumentTitle('Личный кабинет')
+    useDocumentTitle('Личный кабинет')
 
-  return (
-    <main className={styles.page}>
-      <DashboardOverview />
-      <DashboardRatingChart />
-      <DashboardScenariosBarChart />
-      <DashboardBalanceChart />
-      <DashboardHeatMap />
-    </main>
-  )
+    return (
+        <main className={styles.page}>
+            <DashboardOverview/>
+            <DashboardRatingChart/>
+            <DashboardBalanceChart/>
+            <DashboardScenariosBarChart/>
+            <DashboardPathProgress/>
+        </main>
+    )
 }
