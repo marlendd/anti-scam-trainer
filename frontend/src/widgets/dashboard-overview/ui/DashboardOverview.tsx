81ca337fdeaacde888e71daa0471b8e868cc3ac4
Егор Лапшин <https.abuser@proton.me>
2026-08-09 12:01:31 +0300

    chore(frontend): update alpine node & npm versions

--- frontend/src/widgets/dashboard-overview/ui/DashboardOverview.tsx
+++ frontend/src/widgets/dashboard-overview/ui/DashboardOverview.tsx
@@ -1,15 +1,33 @@
 import styles from './DashboardOverview.module.scss'
 import { Avatar } from '@/shared/ui/avatar'
 import { LogoutButton } from '@/features/logout-button'
+import {useCurrentUser} from "@/entities/user";
 
 export function DashboardOverview() {
+    const {
+        data: user,
+        isLoading,
+        isError,
+    } = useCurrentUser()
+
+    if (isLoading) {
+        return null
+    }
+
+    if (isError || !user) {
+        return null
+    }
+
     return (
         <section className={styles.overview}>
             <div className={styles.user}>
                 <Avatar size={120} />
 
                 <div className={styles.text}>
-                    <p className={styles.name}>Unknown User</p>
+                    <p className={styles.name}>
+                        {user.email}
+                    </p>
+
                     <p className={styles.rank}>
                         <LogoutButton />
                     </p>
